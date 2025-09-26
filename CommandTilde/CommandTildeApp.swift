5e7d33a66126af8c5b9908c82df01e090c1f0b22
Vladyslav Shvedov <mail@vlad.codes>
2025-09-25 17:03:03 -0700

    Initial Commit

---
+++ CommandTilde/CommandTildeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommandTildeApp.swift
+//  CommandTilde
+//
+//  Created by vsh on 2025-09-25.
+//
+
+import SwiftUI
+
+@main
+struct CommandTildeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
